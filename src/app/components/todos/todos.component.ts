96464132f010869183c003293a3aa9a64e165b62
byemane <beteyemane@hotmail.co.uk>
2019-04-19 16:34:27 +0100

    main front-end & backend done

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+     this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    })
+  }
+
+  deleteTodo(todo:Todo) {
+    //remove todo from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    //remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+}
